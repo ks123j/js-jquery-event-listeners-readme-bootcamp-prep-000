9feaf0228f168adb7063748cfb5f70ea360a47e1
Katy <komal.vadukul@gmail.com>
2017-11-28 20:37:49 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -4,25 +4,6 @@ function getIt() {
 });
 }
 
-function frameIt() {
-  $('img').on('load', function(){
-  $('img').addClass('tasty');
-});
-}
-
-function pressIt() {
-  $(document).on('keydown', function(key) {
-  if(key.which == 71){
-    alert('G was pressed');
-  }
-});
-}
-
-function submitIt() {
-  $("form").on("submit", function() {
-    alert('your form is going to be submitted now');
-  }
-}
 
 
 
